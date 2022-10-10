5fb22344321c9c2c0055ffcd5cba1bcf211cdbd6
Ede Mounika <edemounika99@gmail.com>
2022-10-10 10:35:47 +0530

    loops

--- loops.js
+++ loops.js
@@ -17,12 +17,12 @@ while(x<=5)
 
 //for loop
 
-for(let i=1;i<=5,i++;)
+/*for(let i=1;i<=5,i++;)
 {
     console.log("print me",i);
     for(let j=1;j<=5;j++)
          console.log("hi",j)   
-}
+}*/
 
 
 
